7881be5e2c4125841dacd6e41757be342a6b3abc
s523368 <s523368@nwmissouri.edu>
2018-04-13 00:49:44 -0500

    figured out how to color and made some functions
    algo not implemented yet

--- graphcoloring.py
+++ graphcoloring.py
@@ -2,21 +2,52 @@
 # Author: Anthony Enriquez, Sumnima Rana, David Schmitt
 # Description: Final Project
 # Due: 4/20/18
-# I pledge that I have completed the programming assignment independently.
-# I have not copied the code from a student or any source.
-# I have not given my code to any other student.
-# I have not given my code to any other student and will not share this code with anyone under any circumstances.
+# We pledge that we have completed the programming assignment independently.
+# We have not copied the code from a student or any source.
+# We have not given my code to any other student.
+# We have not given my code to any other student and will not share this code with anyone under any circumstances.
 
 import networkx as nx
 import matplotlib.pyplot as plt
+from operator import itemgetter
 
 
 
+def color(G):
+    # sort nodes by degree, dec value (value,deg)
+    sortednodes = sorted(G.degree, key=itemgetter(1), reverse=True)
+    newnodelist = [a[0] for a in sortednodes]
+
+    G2 = nx.Graph();
+    G2.add_nodes_from(newnodelist, color='null')
+    G2.add_edges_from(G.edges)
+
+    print(newnodelist[0])
+
+    #print(G2.node[newnodelist[0]]['color'])
+    for node in newnodelist[1:]:
+        print(G2.nodes[node])
+    return G2
+
+
+def draw(G):
+    # Position nodes using Fruchterman-Reingold force-directed algorithm
+    pos = nx.spring_layout(G)
+    colors = ['blue', 'red', 'yellow', 'green', 'orange', 'purple']
+    # draw graph and us matplotlib to visualize
+    nx.draw(G, pos, with_labels=True, node_color= colors)
+
+# test
+
+# init graph
 G = nx.Graph()
 
-G.add_edge(1, 2)
-G.add_edge(2,3)
-pos = nx.spring_layout(G)
+# create list of nodes, edges and add to G
+nodelist = ['A', 'B', 'C', 'D', 'E', 'F']
+edgelist = [('A','B'),('B','C'),('B','D'), ('C', 'D'), ('C', 'E'), ('C', 'F')]
+G.add_nodes_from(nodelist)
+G.add_edges_from(edgelist)
 
-nx.draw(G,pos)
+coloredgraph = color(G)
+draw(coloredgraph)
 plt.show()
